35a69d60ae40814692357a19a28e8adebc6d5f59
what-in-the-nim <chatcharinsang@gmail.com>
2024-01-15 01:49:26 +0700

    Refactor jit module

--- infer/lib/jit/__init__.py
+++ infer/lib/jit/__init__.py
@@ -7,71 +7,44 @@ import torch
 from tqdm import tqdm
 
 
-def load_inputs(path, device, is_half=False):
-    parm = torch.load(path, map_location=torch.device("cpu"))
-    for key in parm.keys():
-        parm[key] = parm[key].to(device)
-        if is_half and parm[key].dtype == torch.float32:
-            parm[key] = parm[key].half()
-        elif not is_half and parm[key].dtype == torch.float16:
-            parm[key] = parm[key].float()
-    return parm
+def load_inputs(path: str, device: str = "cpu", is_half: bool = False) -> dict:
+    """Load inputs from given path and cast to device."""
+    # Load parmeters into CPU first.
+    params = torch.load(path, map_location="cpu")
+    # Iterate through parameters
+    for key in params:
+        # Cast parameter to device
+        params[key] = params[key].to(device)
+        # If half precision is enabled and parameter is float32, cast to float16
+        if is_half and params[key].dtype == torch.float32:
+            params[key] = params[key].half()
+        # If half precision is disabled and parameter is float16, cast to float32
+        elif not is_half and params[key].dtype == torch.float16:
+            params[key] = params[key].float()
+    return params
 
 
 def benchmark(
-    model, inputs_path, device=torch.device("cpu"), epoch=1000, is_half=False
-):
-    parm = load_inputs(inputs_path, device, is_half)
+    model,
+    inputs_path: str,
+    device: str = "cpu",
+    epoch: int = 1000,
+    is_half: bool = False,
+) -> None:
+    """Benchmark model inference time."""
+    # Load model inputs
+    inputs = load_inputs(inputs_path, device, is_half)
+    # Set the time counter to zero.
     total_ts = 0.0
-    bar = tqdm(range(epoch))
-    for i in bar:
+    for _ in tqdm(range(epoch)):
+        # Measure inference time.
         start_time = time.perf_counter()
-        o = model(**parm)
-        total_ts += time.perf_counter() - start_time
+        model(**inputs)
+        stop_time = time.perf_counter()
+        # Accumulate inference time.
+        total_ts += stop_time - start_time
+
     print(f"num_epoch: {epoch} | avg time(ms): {(total_ts*1000)/epoch}")
-
-
-def jit_warm_up(model, inputs_path, device=torch.device("cpu"), epoch=5, is_half=False):
-    benchmark(model, inputs_path, device, epoch=epoch, is_half=is_half)
-
-
-def to_jit_model(
-    model_path,
-    model_type: str,
-    mode: str = "trace",
-    inputs_path: str = None,
-    device=torch.device("cpu"),
-    is_half=False,
-):
-    model = None
-    if model_type.lower() == "synthesizer":
-        from .get_synthesizer import get_synthesizer
-
-        model, _ = get_synthesizer(model_path, device)
-        model.forward = model.infer
-    elif model_type.lower() == "rmvpe":
-        from .get_rmvpe import get_rmvpe
-
-        model = get_rmvpe(model_path, device)
-    elif model_type.lower() == "hubert":
-        from .get_hubert import get_hubert_model
-
-        model = get_hubert_model(model_path, device)
-        model.forward = model.infer
-    else:
-        raise ValueError(f"No model type named {model_type}")
-    model = model.eval()
-    model = model.half() if is_half else model.float()
-    if mode == "trace":
-        assert not inputs_path
-        inputs = load_inputs(inputs_path, device, is_half)
-        model_jit = torch.jit.trace(model, example_kwarg_inputs=inputs)
-    elif mode == "script":
-        model_jit = torch.jit.script(model)
-    model_jit.to(device)
-    model_jit = model_jit.half() if is_half else model_jit.float()
-    # model = model.half() if is_half else model.float()
-    return (model, model_jit)
 
 
 def export(
