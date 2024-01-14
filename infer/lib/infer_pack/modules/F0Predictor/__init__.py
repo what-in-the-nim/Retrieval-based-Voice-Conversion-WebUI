d1fd9372f5866b5e7094f89fd897b6c4987c5cf7
what-in-the-nim <chatcharinsang@gmail.com>
2024-01-14 23:04:20 +0700

    Implement F0PredictorFactory

--- infer/lib/infer_pack/modules/F0Predictor/__init__.py
+++ infer/lib/infer_pack/modules/F0Predictor/__init__.py
@@ -1,5 +1,18 @@
+from .base import F0Predictor
 from .dio import DioF0Predictor
 from .harvest import HarvestF0Predictor
 from .pm import PMF0Predictor
 
-__all__ = ["DioF0Predictor", "HarvestF0Predictor", "PMF0Predictor"]
+class F0PredictorFactory:
+    @staticmethod
+    def create(predictor_type) -> F0Predictor:
+        if predictor_type == "dio":
+            return DioF0Predictor()
+        elif predictor_type == "harvest":
+            return HarvestF0Predictor()
+        elif predictor_type == "pm":
+            return PMF0Predictor()
+        else:
+            raise ValueError("Invalid predictor type")
+
+__all__ = ["F0PredictorFactory", "DioF0Predictor", "HarvestF0Predictor", "PMF0Predictor"]
