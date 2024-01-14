b6f6ab6a747ef7e848ea8c7bee7bfaf0f436f010
what-in-the-nim <chatcharinsang@gmail.com>
2024-01-14 22:42:06 +0700

    Refactor get_rmvpe.py

--- infer/lib/jit/get_rmvpe.py
+++ infer/lib/jit/get_rmvpe.py
@@ -1,12 +1,19 @@
 import torch
 
+from infer.lib.rmvpe import E2E
 
-def get_rmvpe(model_path="assets/rmvpe/rmvpe.pt", device=torch.device("cpu")):
-    from infer.lib.rmvpe import E2E
-
+def get_rmvpe(
+    path: str = "assets/rmvpe/rmvpe.pt",
+    device: str | torch.device = "cpu",
+) -> E2E:
+    """Load RMVPE model from given path and cast to device."""
+    # Initialize model
     model = E2E(4, 1, (2, 2))
-    ckpt = torch.load(model_path, map_location=device)
-    model.load_state_dict(ckpt)
+    # Load model state dict to model
+    state_dict = torch.load(path, map_location=device)
+    model.load_state_dict(state_dict)
+    # Set model to eval mode.
     model.eval()
+    # Cast model to device
     model = model.to(device)
     return model
